00b4aefa7eacdacca8437a219853845d40718b6d
Jason Hibbs <jason@jasonhibbs.co.uk>
2020-05-28 13:55:33 +0100

    add loader to button

--- src/components/ButtonLocation.vue
+++ src/components/ButtonLocation.vue
@@ -2,31 +2,41 @@
 
   button.button-location(
     v-if="hasGeolocation"
-    title="Get current location"
+    :title="title"
     @click="getCurrentLocation"
   )
-    inline-svg(:src="`/location-arrow.svg`")
+    template(v-if="!isFinding")
+      inline-svg(:src="`/location-arrow.svg`")
+    template(v-else)
+      loader
 
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import { mapState } from 'vuex'
 import InlineSvg from 'vue-inline-svg'
+import Loader from '@/components/Loader.vue'
 
 @Component({
-  components: { InlineSvg },
+  components: { InlineSvg, Loader },
   computed: {
     ...mapState(['mapView']),
   },
 })
 export default class ButtonLocation extends Vue {
   mapView!: any
+  isFinding: boolean = false
+
+  get title() {
+    return this.isFinding ? 'Getting location…' : 'Get current location'
+  }
 
   get hasGeolocation() {
     return !!navigator.geolocation
   }
 
   getCurrentLocation() {
+    this.isFinding = true
     navigator.geolocation.getCurrentPosition(
       this.updateCurrentLocation,
       this.handleLocationError
@@ -36,11 +46,12 @@ export default class ButtonLocation extends Vue {
   updateCurrentLocation(position: Position) {
     this.mapView.context.lat = position.coords.latitude
     this.mapView.context.lng = position.coords.longitude
-    this.$emit('originclick')
+    this.isFinding = false
   }
 
   handleLocationError(error: PositionError) {
     console.warn(error.message)
+    this.isFinding = false
   }
 }
 </script>
---
+++ src/components/Loader.vue
@@ -0,0 +1,70 @@
+<template lang="pug">
+
+  span.loader
+    span.loader-spinner(:style="sizeStyle")
+      span(:style="colorStyle")
+    span.loader-label
+      slot
+
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Loader extends Vue {}
+</script>
+<style lang="scss">
+.loader {
+  --size: 1.25em;
+  --width: 2px;
+  --light-color: var(--tint-mid);
+  --color: var(--color-tint);
+  --speed: #{(2s/3)};
+
+  display: flex;
+  align-items: center;
+  color: currentColor;
+
+  .loader-spinner,
+  .loader-label {
+    flex: none;
+  }
+
+  .loader-spinner {
+    margin: auto;
+
+    > span {
+      display: block;
+      width: var(--size);
+      height: var(--size);
+      border: var(--width) solid var(--color);
+      border-top-color: var(--light-color) !important;
+      border-radius: var(--size);
+      animation: spin var(--speed) linear infinite;
+    }
+  }
+
+  .loader-label:not(:empty) {
+    margin-left: 0.5em;
+  }
+
+  @keyframes spin {
+    from {
+      transform: rotateZ(0deg);
+    }
+    to {
+      transform: rotateZ(360deg);
+    }
+  }
+}
+
+[data-loading] {
+  transition: opacity 0.2s;
+  will-change: opacity;
+}
+
+[data-loading='true'] {
+  opacity: (1/3);
+}
+</style>
