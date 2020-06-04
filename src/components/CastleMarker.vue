3f024e6620b1dd7e6557dec3191190d8568ebda3
Jason Hibbs <jason@jasonhibbs.co.uk>
2020-06-04 15:40:37 +0100

    undelete castle marker, just in case

---
+++ src/components/CastleMarker.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+
+  .marker.castle-marker
+    .marker-pin
+      .marker-head
+      .marker-shaft
+    .marker-label
+      .marker-label-content {{ label }}
+
+
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class CastleMarker extends Vue {
+  @Prop() castle!: any
+
+  get label() {
+    return this.castle.name
+  }
+}
+</script>
